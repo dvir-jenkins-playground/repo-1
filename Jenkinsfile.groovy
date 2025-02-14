ca2a28e500c4f7a32ecde238c7d5b7ab52c57983
dvirlabs <dvirlabs@gmail.com>
2025-02-14 15:30:03 +0200

    Change sleep to 15

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,7 +4,7 @@ pipeline {
         stage('Build') {
             steps {
                 echo 'Building..'
-                sleep 20
+                sleep 15
             }
         }
         stage('Test') {
